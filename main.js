dccf1ccee2228b688c6be4cc06add22525f59acb
Mateusz Młynek <mati8765432a@gmail.com>
2022-01-30 00:06:16 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,117 @@
+const pwEl = document.getElementById('pw')
+const copyEl = document.getElementById('copy')
+const lengthEl = document.getElementById('length')
+const upperEl = document.getElementById('upper')
+const lowerEl = document.getElementById('lower')
+const numberEl = document.getElementById('number')
+const symbolEl = document.getElementById('symbol')
+const generateEl = document.getElementById('generate')
+
+const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols = '!@#$%^&*()_+=';
+
+
+function getUppercase() {
+    return upperLetters[Math.floor(Math.random() * upperLetters.length)]
+}
+function getLowercase() {
+    return lowerLetters[Math.floor(Math.random() * lowerLetters.length)]
+}
+function getNumber() {
+    return numbers[Math.floor(Math.random() * numbers.length)]
+}
+function getSymbol() {
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+
+function generatePassword() {
+    const length = lengthEl.value;
+
+    let password = "";
+
+    if (upperEl.checked) {
+        password += getUppercase();
+    }
+
+    if (lowerEl.checked) {
+        password += getLowercase();
+    }
+
+    if (numberEl.checked) {
+        password += getNumber();
+    }
+
+    if (symbolEl.checked) {
+        password += getSymbol();
+    }
+
+    for (let i = password.length; i < length; i++) {
+        const x = generateX();
+        password += x;
+    }
+
+    pwEl.innerText = password;
+}
+
+
+function generateX() {
+    const xs = [];
+    if (upperEl.checked) {
+        xs.push(getUppercase());
+    }
+    if (lowerEl.checked) {
+        xs.push(getLowercase());
+    }
+    if (numberEl.checked) {
+        xs.push(getNumber());
+    }
+    if (symbolEl.checked) {
+        xs.push(getSymbol());
+    }
+    if (xs.length === 0) return ""
+
+    return xs[Math.floor(Math.random() * xs.length)];
+
+}
+
+generateEl.addEventListener('click', generatePassword);
+
+
+copyEl.addEventListener("click", () => {
+    const textarea = document.createElement("textarea");
+    const password = pwEl.innerText;
+
+    if (!password) {
+        return;
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    textarea.remove();
+});
+
+
+
+
+
+// Select the button
+const btn = document.querySelector(".mode");
+// Select the stylesheet <link>
+const theme = document.querySelector("#theme-link");
+
+// Listen for a click on the button
+btn.addEventListener("click", function () {
+    // If the current URL contains "ligh-theme.css"
+    if (theme.getAttribute("href") == "dark-theme.css") {
+        theme.href = "light-theme.css";
+        btn.innerHTML = `Light mode`;
+    } else {
+        theme.href = "dark-theme.css";
+        btn.innerHTML = `Dark mode`;
+    }
+});
